637bcfe584d0c5763d9faeb3a857059abbbad9a3
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-21 20:01:20 +0700

    refactor(card-subject): improve for readability

--- src/libs/components/scheduleTable/CardSubject.svelte
+++ src/libs/components/scheduleTable/CardSubject.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
-  import type { ScheduleI } from "../../utils/StudentScraping";
-  export let subject: ScheduleI;
+  import type { ScheduleI } from '../../utils/StudentScraping'
+  export let subject: ScheduleI
+
+  const getSectionType = () =>
+    subject.time.type === 'ทฤษฏี' ? subject.theory : subject.practice
+  const displayRoom = () => (subject.room ? `( ${subject.room} )` : '')
 </script>
 
 <div
   class="bg-orange-100/50 border-l-2 border-orange-200 px-3 py-1 h-full flex flex-col justify-between rounded
-  hover:scale-105 hover:border-orange-400 hover:rotate-2 transition-all"
+  hover:border-orange-400 hover:scale-105 hover:rotate-2 transition-all"
 >
-  <div class="flex justify-between text-xs opacity-65 z-10">
+  <div class="flex justify-between text-xs text-gray-600">
     <p>{subject.time.type}</p>
     <p class="whitespace-nowrap">
-      {subject.time.start} -
-      {subject.time.end}
+      {subject.time.start} - {subject.time.end}
     </p>
   </div>
   <h3 class="text-orange-400 text-left text-sm">{subject.name}</h3>
-  <div class="flex justify-between text-xs opacity-65">
+  <div class="flex justify-between text-xs text-gray-600">
     <p class="whitespace-nowrap">
-      section({subject.time.type == "ทฤษฏี"
-        ? subject.theory
-        : subject.practice})
+      section({getSectionType()})
     </p>
     <p class="whitespace-nowrap">
-      {subject.building}
-      {subject.room == "" ? "" : `( ${subject.room} )`}
+      {subject.building}{displayRoom()}
     </p>
   </div>
 </div>
