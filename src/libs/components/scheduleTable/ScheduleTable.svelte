bc20779fa9f8610d630d30b5b08ca3d8933e5afd
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-21 20:06:09 +0700

    fix(schedule-table): improve table accessibility component

--- src/libs/components/scheduleTable/ScheduleTable.svelte
+++ src/libs/components/scheduleTable/ScheduleTable.svelte
@@ -1,53 +1,57 @@
 <script lang="ts">
-  import { createTimeSlot } from "../../utils/StudentHelper";
-  import type { ScheduleI } from "../../utils/StudentScraping";
-  import CardSubject from "./CardSubject.svelte";
+  import { createTimeSlot } from '../../utils/StudentHelper'
+  import type { ScheduleI } from '../../utils/StudentScraping'
+  import CardSubject from './CardSubject.svelte'
 
-  export let schedule: Array<ScheduleI>;
+  export let schedule: Array<ScheduleI>
 
   const days = [
-    { name: "Mon", code: "จ." },
-    { name: "Tue", code: "อ." },
-    { name: "Wed", code: "พ." },
-    { name: "Thu", code: "พฤ." },
-    { name: "Fri", code: "ศ." },
-    { name: "Sat", code: "ส." },
-    { name: "Sun", code: "อา." },
-  ];
+    { name: 'Mon', code: 'จ.' },
+    { name: 'Tue', code: 'อ.' },
+    { name: 'Wed', code: 'พ.' },
+    { name: 'Thu', code: 'พฤ.' },
+    { name: 'Fri', code: 'ศ.' },
+    { name: 'Sat', code: 'ส.' },
+    { name: 'Sun', code: 'อา.' }
+  ]
 
-  const formatTime = (hour: number) => `${hour.toString().padStart(2, "0")}:00`;
+  const hours = Array.from({ length: 20 - 8 }, (_, i) => i + 8)
+  const formatTime = (hour: number) => `${hour.toString().padStart(2, '0')}:00`
 </script>
 
 <div>
-  <table class="w-full rounded-2xl" cellspacing={44}>
+  <table class="w-full rounded-2xl table-auto" cellspacing={44}>
     <thead>
       <tr>
-        <th />
-        {#each Array.from({ length: 20 - 8 }, (_, i) => i + 8) as hour}
+        <th scope="col" />
+        {#each hours as hour (hour)}
           {#if hour < 20 - 1}
-            <th class="border-x border-orange-100 w-[8.33%]" colspan="4">
+            <th
+              scope="col"
+              class="border-x border-orange-100 w-[8.33%]"
+              colspan="4"
+            >
               {`${formatTime(hour)} - ${formatTime(hour + 1)}`}
             </th>
           {/if}
         {/each}
-      </tr>
-    </thead>
+      </tr></thead
+    >
     <tbody>
-      {#each days as day}
+      {#each days as day (day.name)}
         <tr class="hover:bg-orange-100/20 group">
-          <td
+          <th
+            scope="row"
             class="text-right font-semibold text-sm text-orange-300 whitespace-nowrap group-hover:scale-125 group-hover:text-orange-400 transition-all"
           >
             {day.name}
-          </td>
+          </th>
           {#each createTimeSlot(schedule, day.code) as slot}
-            {#if slot == undefined}
-              <td class="border-x border-orange-100" />
-            {:else}
-              <td colspan={slot.colSpan}>
+            <td class="border-x border-orange-100" colspan={slot?.colSpan || 1}>
+              {#if slot}
                 <CardSubject subject={slot} />
-              </td>
-            {/if}
+              {/if}
+            </td>
           {/each}
         </tr>
       {/each}
@@ -56,10 +60,14 @@
 </div>
 
 <style scoped>
+  th,
+  td {
+    @apply font-prompt;
+  }
   th {
-    @apply font-prompt font-normal whitespace-nowrap text-orange-400;
+    @apply font-normal whitespace-nowrap text-orange-400;
   }
   td {
-    @apply font-prompt w-[2.22%] p-1 h-28;
+    @apply w-[2.22%] p-1 h-28;
   }
 </style>
