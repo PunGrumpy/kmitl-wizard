56d1d4dbeee9b9be637751be5ee87dc317c24281
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-21 20:24:47 +0700

    style(card-subject): change text color

--- src/libs/components/scheduleTable/CardSubject.svelte
+++ src/libs/components/scheduleTable/CardSubject.svelte
@@ -10,19 +10,20 @@
 <div
   class="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow flex flex-col justify-between h-full"
 >
-  <div class="flex justify-between text-xs text-gray-600">
+  <div class="flex justify-between text-md text-gray-400">
     <p>{subject.time.type}</p>
     <p class="whitespace-nowrap">
       {subject.time.start} - {subject.time.end}
     </p>
   </div>
   <h3 class="text-orange-500 font-bold text-lg">{subject.name}</h3>
-  <div class="flex justify-between text-xs text-gray-600">
+  <div class="flex justify-between text-md text-gray-600">
     <p class="whitespace-nowrap">
       section({getSectionType()})
     </p>
     <p class="whitespace-nowrap">
-      {subject.building}{displayRoom()}
+      {subject.building}
+      {displayRoom()}
     </p>
   </div>
 </div>
