f4dfbe860597b9dd9c5cb950f667dddc448f4e09
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-25 01:55:17 +0700

    chore(vite): add auto zip

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { crx } from '@crxjs/vite-plugin'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import AutoZip from 'vite-plugin-auto-zip'
 import manifest from './src/manifest.config'
 
 export default defineConfig({
-  plugins: [svelte(), crx({ manifest })],
+  plugins: [svelte(), crx({ manifest }), AutoZip()],
   server: {
     port: 5173,
     strictPort: true,
