d5553a7a2c8adf3285d406909f706711e91d81b6
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-25 15:08:23 +0700

    chore(vite): change lib and custom file name

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { crx } from '@crxjs/vite-plugin'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
-import AutoZip from 'vite-plugin-auto-zip'
+import zipPack from 'vite-plugin-zip-pack'
 import manifest from './src/manifest.config'
 
 export default defineConfig({
-  plugins: [svelte(), crx({ manifest }), AutoZip()],
+  plugins: [
+    svelte(),
+    crx({ manifest }),
+    zipPack({
+      inDir: 'dist',
+      outDir: 'dist',
+      outFileName: 'kmitl-wizard.zip'
+    })
+  ],
   server: {
     port: 5173,
     strictPort: true,
