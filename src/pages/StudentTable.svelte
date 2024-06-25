f2cdc56e50093c5e6ce8f2654a797c1f9b4645cd
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-25 22:59:49 +0700

    feat: add check new version

--- src/pages/StudentTable.svelte
+++ src/pages/StudentTable.svelte
@@ -7,6 +7,8 @@
   import ScheduleTable from '../components/ScheduleTable.svelte'
   import ThemeToggle from '../components/ThemeToggle.svelte'
   import Toast from '../components/Toast.svelte'
+  import packageJson from '../../package.json'
+  import { onMount } from 'svelte'
 
   export let schedule: Array<ScheduleI>
   export let information: InformationI
@@ -14,6 +16,8 @@
   let captureScreen: HTMLDivElement
   let toastMessage: string = ''
   let showToast: boolean = false
+  let showUpdateNotification: boolean = false
+  let latestVersion: string = ''
 
   const showNotification = (message: string) => {
     toastMessage = message
@@ -45,11 +49,42 @@
     )
     showNotification('PNG exported successfully!')
   }
+
+  const checkUpdateVersion = async () => {
+    const res = await fetch(
+      'https://api.github.com/repos/PunGrumpy/kmitl-wizard/releases/latest'
+    )
+    const data = await res.json()
+    latestVersion = data.tag_name
+    const currentVersion = `v${packageJson.version}`
+    if (latestVersion !== currentVersion) {
+      showUpdateNotification = true
+      showNotification(`New version available: ${latestVersion}`)
+    }
+  }
+
+  onMount(async () => {
+    await checkUpdateVersion()
+  })
 </script>
 
 <main
   class="min-h-screen p-6 flex flex-col justify-between bg-zinc-100 dark:bg-zinc-900"
 >
+  {#if showUpdateNotification}
+    <div
+      class="bg-orange-500 text-white dark:bg-orange-600 dark:text-black text-center p-4 rounded-lg mb-4"
+    >
+      New version available: <a
+        href="https://github.com/PunGrumpy/kmitl-wizard/releases/tag/{latestVersion}"
+        class="underline hover:text-orange-200 dark:hover:text-orange-400 transition-colors"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {latestVersion}
+      </a>
+    </div>
+  {/if}
   <div
     bind:this={captureScreen}
     class="backdrop-blur-xs bg-white-70 dark:bg-black-70 p-4 shadow-lg rounded-lg"
