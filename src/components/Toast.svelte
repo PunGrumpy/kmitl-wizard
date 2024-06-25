c500bea9e6c6334ffc8cbb037d6216e6d59cd0f6
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-25 18:53:44 +0700

    feat(toast): add click to close

--- src/components/Toast.svelte
+++ src/components/Toast.svelte
@@ -1,20 +1,39 @@
 <script lang="ts">
   import { onMount } from 'svelte'
+  import { tick } from 'svelte'
+
   export let message: string
   export let duration: number = 3000
 
   let visible = false
+  let timeoutId: NodeJS.Timeout
 
-  onMount(() => {
+  onMount(async () => {
     visible = true
-    setTimeout(() => {
+
+    timeoutId = setTimeout(() => {
       visible = false
     }, duration)
+
+    await tick()
   })
+
+  function closeToast() {
+    clearTimeout(timeoutId)
+    visible = false
+  }
 </script>
 
-<div class="toast {visible ? 'visible' : ''}">
-  {message}
+<div>
+  <button
+    class="toast {visible
+      ? 'visible'
+      : ''} relative cursor-pointer focus:outline-none"
+    on:click={closeToast}
+    aria-label="Close notification"
+  >
+    {message}
+  </button>
 </div>
 
 <style>
@@ -23,8 +42,9 @@
     bottom: 1rem;
     right: 1rem;
     padding: 0.75rem 1.25rem;
-    background-color: #333;
-    color: #fff;
+    background-color: rgba(0, 0, 0, 0.65);
+    backdrop-filter: blur(0.5rem);
+    color: rgba(255, 255, 255, 0.9);
     border-radius: 0.5rem;
     box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
     opacity: 0;
